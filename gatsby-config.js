edace0fd10360222884b8319c1afba8104d3c4e4
Enjeck <patrathewhiz@gmail.com>
2021-09-17 04:23:41 +0100

    Use custom favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sass`,
-    `gatsby-plugin-stripe`,
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-webfonts`,
@@ -40,7 +39,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // path is relative to the root of the site.
+        icon: `src/images/favicon-32x32.png`, // path is relative to the root of the site.
       },
     },
   ],
