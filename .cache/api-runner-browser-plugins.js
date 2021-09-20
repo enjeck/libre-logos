6de14e999b1801b52a5fa7d6a79f03ec019d1198
E. Cleopatra <32180937+PROTechThor@users.noreply.github.com>
2021-09-20 08:31:11 +0100

    Delete .cache directory

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/favicon-32x32.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"0358a3b44dffc2b26de3f010d8ccae64"},
-    }]
