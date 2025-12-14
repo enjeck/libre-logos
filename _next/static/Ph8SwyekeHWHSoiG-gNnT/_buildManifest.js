c4444fb3e2964ef89f95c56a7514938e509366d0
Enjeck C. <patrathewhiz@gmail.com>
2025-12-14 11:17:03 +0000

    Updates

---
+++ _next/static/Ph8SwyekeHWHSoiG-gNnT/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/libre-logos//_next/:path+",
+        "destination": "/libre-logos/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/Ph8SwyekeHWHSoiG-gNnT/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Flogo\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
